c47a38d493982e38a9917bc4d7dcbd7ebc34d868
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2024-10-21 15:17:21 +1300

    Create mcmc_runner.py

---
+++ mcmc_runner.py
@@ -0,0 +1,17 @@
+# import SGVB's Bayesian model
+# import SGVB sim varma
+# import emcee (https://emcee.readthedocs.io/en/stable/index.html)
+
+class McmcRunner:
+  pass
+
+
+def main():
+  # simulate VAR data
+  # use McmcRunner(data)
+  # plot PSD + true
+  pass
+
+
+if __name__ == "__main__:
+  pass
